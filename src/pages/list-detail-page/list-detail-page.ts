09e85c74ff6ec9c99d8fa6630a4b54b1cd414ac5
yngvelokas <yngve.lokas@live.no>
2017-03-21 13:08:39 +0100

    Forsøk på "add items"

--- src/pages/list-detail-page/list-detail-page.ts
+++ src/pages/list-detail-page/list-detail-page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavParams } from 'ionic-angular';
+import { ModalController, NavController, reorderArray } from 'ionic-angular';
+import { Data } from '../../providers/data';
 import { AddItemPage } from '../add-item-page/add-item-page'
 
 
@@ -11,14 +13,44 @@ export class ListDetailPage {
 
   title;
   description;
+  public items = [];
 
-  constructor(public navParams: NavParams) {
+  constructor(public navParams: NavParams, navCtrl: NavController, public modalCtrl: ModalController, public dataService: Data) {
 
   }
 
   ionViewDidLoad() {
     this.title = this.navParams.get('list').title;
     this.description = this.navParams.get('list').description;
+  }
+
+  addItem() {
+
+    let addModal = this.modalCtrl.create(AddItemPage);
+    // call back when modal dismissed
+    addModal.onDidDismiss((item) => {
+      if (item) {
+        this.saveItem(item);
+      }
+    });
+    addModal.present();
+
+  }
+
+    saveItem(item) {
+    this.items.push(item);
+    this.dataService.save(this.items);
+  }
+
+    reorderItems(indexes){
+    this.items = reorderArray(this.items, indexes);
+  }
+
+    deleteItem(item){
+    let index = this.items.indexOf(item);
+    if(index > -1){
+      this.items.splice(index, 1);
+    }
   }
 
 }
--- src/providers/data.ts
+++ src/providers/data.ts
@@ -16,5 +16,6 @@ export class Data {
     let newData = JSON.stringify(data);
     this.storage.set('handlelapp', newData);
   }
+  
 
 }
